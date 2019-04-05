a170d22f89e4307571966313944504eb1e2cb5f7
Daniel Poulson <danielpoulson@hotmail.com.au>
2019-04-05 13:58:33 +1100

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,148 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "../styles/page1.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const totalItems = 5
+
+class IndexPage extends React.Component {
+  state = {
+    items: ["active", "next", "", "", "prev"],
+    moving: false,
+    slide: 1,
+  }
+
+  componentWillMount() {
+    window.addEventListener("keydown", this.enterToSearch)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListner("keydown", this.enterToSearch)
+  }
+
+  enterToSearch = event => {
+    if (event.keyCode === 37) {
+      this.movePrev()
+    }
+    if (event.keyCode === 39 || event.keyCode === 13) {
+      this.moveNext()
+    }
+  }
+
+  moveNext() {
+    let moveTo = this.state.slide
+    // Check if moving
+    if (!this.state.moving) {
+      // If it's the last slide, reset to 0, else +1
+      if (this.state.slide === totalItems) {
+        moveTo = 1
+      } else {
+        moveTo = this.state.slide + 1
+      }
+      // Move carousel to updated slide
+      this.setState({ slide: moveTo })
+      this.moveCarouselTo(moveTo)
+    }
+  }
+
+  movePrev() {
+    let moveTo = this.state.slide
+    const moving = this.state.moving
+    // Check if moving
+    if (!moving) {
+      // If it's the last slide, reset to 0, else +1
+      if (this.state.slide === 1) {
+        moveTo = totalItems
+      } else {
+        moveTo = this.state.slide - 1
+      }
+      // Move carousel to updated slide
+      this.setState({ slide: moveTo })
+      this.moveCarouselTo(moveTo)
+    }
+  }
+
+  disableInteraction() {
+    // Set 'moving' to true for the same duration as our transition.
+    // (0.5s = 500ms)
+
+    this.setState({ moving: true })
+    // setTimeout runs its function once after the given time
+    setTimeout(() => {
+      this.setState({ moving: false })
+    }, 500)
+  }
+
+  moveCarouselTo(moveTo) {
+    // Check if carousel is moving, if not, allow interaction
+    if (!this.state.moving) {
+      // temporarily disable interactivity
+      this.disableInteraction()
+      // Update the "old" adjacent slides with "new" ones
+      const prev = moveTo === 1 ? 5 : moveTo - 1
+      const next = moveTo === totalItems ? 1 : moveTo + 1
+
+      const returned = this.state.items.map((item, index) => {
+        let _item = ""
+        switch (index + 1) {
+          case moveTo:
+            _item = "active"
+            break
+          case prev:
+            _item = "prev"
+            break
+          case next:
+            _item = "next"
+            break
+
+          default:
+            _item = ""
+            break
+        }
+
+        return _item
+      })
+
+      this.setState({ items: returned })
+    }
+  }
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <h1>Magic Slider</h1>
+        <div className="carousel-wrapper">
+          <div className="carousel">
+            <div className={`carousel__photo ${this.state.items[0]}`}>
+              Image 1
+            </div>
+            <div className={`carousel__photo ${this.state.items[1]}`}>
+              Image 2
+            </div>
+            <div className={`carousel__photo ${this.state.items[2]}`}>
+              Image 3
+            </div>
+            <div className={`carousel__photo ${this.state.items[3]}`}>
+              Image 4
+            </div>
+            <div className={`carousel__photo ${this.state.items[4]}`}>
+              Image 5
+            </div>
+
+            <div
+              className="carousel__button--next"
+              onClick={() => this.moveNext()}
+            />
+            <div
+              className="carousel__button--prev"
+              onClick={() => this.movePrev()}
+            />
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
